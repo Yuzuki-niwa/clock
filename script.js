5161dfec32be4bc4af215d3748b41f3f3dd3745a
Yuzuki-niwa <b2100994@gl.aiu.ac.jp>
2023-10-12 21:30:08 +0900

    やばい時計の表示非表示切り替え

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-function updateClock() {
-    const clockElement = document.getElementById('clock');
+function updateClock(clockId, pad) {
+    const clockElement = document.getElementById(clockId);
     const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const hours = now.getHours().toString().padStart(2, pad);
+    const minutes = now.getMinutes().toString().padStart(4, pad);
+    const seconds = now.getSeconds().toString().padStart(8, pad);
     const timeString = `${hours}:${minutes}:${seconds}`;
     clockElement.textContent = timeString;
 }
 
-setInterval(updateClock, 1000);
+// 時計1: 桁数2でパディング
+updateClock("clock", '愛');
+
+// 時計2: 桁数4でパディング
+updateClock("yabai_d", 'ゑ');
+
+// 1秒ごとにそれぞれの時計を更新
+setInterval(function() {
+    updateClock("clock", '愛');
+    updateClock("yabai_d", 'ゑ');
+}, 1000);
+
+
+function toggleClockVisibility() {
+    const clock2Element = document.getElementById('yabai_d');
+    const showClockButton = document.getElementById('showClockButton');
+    
+    if (clock2Element.style.display === 'none') {
+        clock2Element.style.display = 'block';
+        showClockButton.textContent = 'やばくない';
+    } else {
+        clock2Element.style.display = 'none';
+        showClockButton.textContent = 'やばい';
+    }
+}
+
+const button = document.getElementById('showClockButton');
+button.addEventListener('click', toggleClockVisibility);
